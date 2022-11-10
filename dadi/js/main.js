24370066f39ae457cde9a1df06d1f2ab0f23c1d8
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-10 20:43:48 +0100

    Creazione html + css

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -7,12 +7,20 @@ console.log("I miei numeri", mieiNumeri);
 let pcNumeri = Math.floor(Math.random() * 6) + 1;
 console.log("I numeri del Computer", pcNumeri);
 
+let ilMio = document.getElementById("textmiei");
+let ilPc = document.getElementById("textpc");
 // Creo la condizione
 
 if(mieiNumeri > pcNumeri){
+    ilMio.append(mieiNumeri);
+    ilPc.append(pcNumeri);
     console.log("Hai Vinto, sei troppo forte!", mieiNumeri);
 } else if (mieiNumeri == pcNumeri){
-    console.log("La Partita è pari", mieiNumeri, pcNumeri)
+    ilMio.append(mieiNumeri);
+    ilPc.append(pcNumeri);
+    console.log("La Partita è pari", mieiNumeri, pcNumeri);
 } else{
+    ilMio.append(mieiNumeri);
+    ilPc.append(pcNumeri);
     console.log("Ha Vinto il Computer, riprova e sarai meno sfortunato!", pcNumeri);
 }
