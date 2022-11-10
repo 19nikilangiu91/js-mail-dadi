51f524105c535d0dbd75c1f09df0f3657632c4e8
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-10 17:26:21 +0100

    Esercizio Completato

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,8 +1,6 @@
 // Chiedere all'utente di inserire il nominativo per vedere se è nella lista
 
- let richiesta = prompt("Inserisci il tuo nome per vedere se sei nella lista");
-
-// console.log(richiesta);
+let richiesta = prompt("Inserisci il tuo nome per vedere se sei nella lista");
 
 // Inserire le persone nella lista
 
@@ -10,19 +8,27 @@ let inLista = ["Luca", "Marco", "Paolo"];
 
 // Inserire le persone non in lista
 
-let outLista = false;
+let possoEntrare = false;
 
 // Creo l'array
 
 for (let i = 0; i < inLista.length; i++){
-
-    // console.log(inLista[i]);
-
+    
     // Creo la condizione
     // Se il nome è in lista la persona sarà vera.
-    if (richiesta === inLista[i]) {
-        console.log("Benvenuto alla festa", richiesta);
 
-    } else (richiesta !== inLista[i])
-        console.log("Non sei nella lista")
+    if (richiesta === inLista[i]){
+        
+        possoEntrare = true;
+    }
+    
+    if (possoEntrare == true){
+        console.log("Complimenti puoi entrare");
+        document.getElementById("title"). innerHTML = "Sei nella lista, puoi entrare!";
+    } else {
+        console.log("Mi dispiace, non puoi entrare");
+        document.getElementById("title"). innerHTML = "Non sei nella lista, puoi entrare!";
+    }
 }
+
+
