8ac4fd1cb73bad0e54679b85b440415bca83e44e
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-10 19:26:15 +0100

    Numeri del Computer

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -1,4 +1,8 @@
 
-// Genero i mie numeri.
+// Genero i miei numeri.
 let mieiNumeri = Math.floor(Math.random() * 6) + 1;
 console.log("I miei numeri", mieiNumeri);
+
+// Genero i numeri del Computer.
+let pcNumeri = Math.floor(Math.random() * 6) + 1;
+console.log("I numeri del Computer", pcNumeri);
