1f5b6f9256477e791a289c61bc9ff23c09078c88
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-10 15:07:28 +0100

    condizione if

--- mail/js/main.js
+++ mail/js/main.js
@@ -1,18 +1,25 @@
 // Inserire le persone nella lista
 
-const inLista = ["Luca", "Marco", "Paolo"];
+let inLista = ["Luca", "Marco", "Paolo"];
 
-console.log("le persone nella lista sono: ", inLista);
+// Inserire le persone non in lista
+
+let outLista = false
+
 
 // Chiedere all'utente di inserire il nominativo per vedere se è nella lista
 
-const richiesta = prompt("Inserisci il tuo nome per vedere se sei nella lista");
+ let richiesta = prompt("Inserisci il tuo nome per vedere se sei nella lista");
 
-console.log(richiesta)
+// console.log(richiesta);
 
 // Creo l'array
 
 for (let i = 0; i < inLista.length; i++){
 
-    console.log(inLista[i]);
+    // Creo la condizione
+    // Se il nome è in lista la persona sarà vera.
+    if (richiesta === inLista[i]) {
+        console.log("Benvenuto alla festa", richiesta);
+    }
 }
