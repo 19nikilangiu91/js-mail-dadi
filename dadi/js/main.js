b8932f6a6f6a46f4271f1f30562aa83a07fad86b
19nikilangiu91 <niki.langiu@gmail.com>
2022-11-10 19:39:14 +0100

    Creazione Condizione Completata

--- dadi/js/main.js
+++ dadi/js/main.js
@@ -6,3 +6,13 @@ console.log("I miei numeri", mieiNumeri);
 // Genero i numeri del Computer.
 let pcNumeri = Math.floor(Math.random() * 6) + 1;
 console.log("I numeri del Computer", pcNumeri);
+
+// Creo la condizione
+
+if(mieiNumeri > pcNumeri){
+    console.log("Hai Vinto, sei troppo forte!", mieiNumeri);
+} else if (mieiNumeri == pcNumeri){
+    console.log("La Partita è pari", mieiNumeri, pcNumeri)
+} else{
+    console.log("Ha Vinto il Computer, riprova e sarai meno sfortunato!", pcNumeri);
+}
